b6d3737ed09385a3152e873bebccd6bc16733dc1
AlejandroQuito <148566826+AlejandroQuito@users.noreply.github.com>
2023-11-12 16:19:20 +0300

    Add files via upload

---
+++ Magnitude.java
@@ -0,0 +1,24 @@
+public class Magnitude {
+    public static void main(String [] args)
+    {
+        byte bMax = Byte.MAX_VALUE;
+        byte bMin = Byte.MIN_VALUE;
+        System.out.println("Byte max:  " + bMax + ", min:  " + bMin);
+        int iMax = Integer.MAX_VALUE;
+        int iMin = Integer.MIN_VALUE;
+        System.out.println("Integer max:  " + iMax + ", min:  " + iMin);
+        short sMax = Short.MAX_VALUE;
+        short sMin = Short.MIN_VALUE;
+        System.out.println("Short max:  " + sMax + ", min:  " + sMin);
+        long lMax = Long.MAX_VALUE;
+        long lMin = Long.MIN_VALUE;
+        System.out.println("Long max:  " + lMax + ", min:  " + lMin);
+        float fMax = Float.MAX_VALUE;
+        float fMin = Float.MIN_VALUE;
+        System.out.println("Float max:  " + fMax + ", min:  " + fMin);
+        double dMax = Double.MAX_VALUE;
+        double dMin = Double.MIN_VALUE;
+        System.out.println("Double max:  " + dMax + ", min:  " + dMin);
+        
+    }
+}
